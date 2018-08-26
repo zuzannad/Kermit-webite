20e5054ea8d1a305383a41f5f5f570e1067b37ab
Zuzanna Dermont <zuzanna.dermont@gmail.com>
2018-08-27 00:12:58 +0200

    DOMContentLoaded event added

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
 
-
-$("#arrow").on("click", function () { 
+$("#arrow").on("click", function () {
 	$("body,html").animate({
 		scrollTop: $("main").offset().top
 	}, 1000)
 })
+});
